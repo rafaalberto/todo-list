c2c60cda51360bee2f77141d1f28b2de62f44366
Rafael Alberto <rafael.alberto1703@gmail.com>
2024-02-17 21:33:35 -0300

    insert info task elements

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -3,10 +3,22 @@ import styles from './Tasks.module.css'
 
 export function Tasks() {
     return (
-        <div className={styles.emptyTask}>
-            <img src={list} alt="Empty List Icon" />
-            <strong>Você ainda não tem tarefas cadastradas</strong>
-            <span>Crie tarefas e organize seus itens a fazer</span>
-        </div>
+        <article className={styles.tasks}>
+            <header className={styles.infoTask}>
+                <aside>
+                    <strong>Tarefas criadas</strong>
+                    <span>0</span>
+                </aside>
+                <aside>
+                    <strong>Concluídas</strong>
+                    <span>0</span>
+                </aside>
+            </header>
+            <div className={styles.emptyTask}>
+                <img src={list} alt="Empty List Icon" />
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+                <span>Crie tarefas e organize seus itens a fazer</span>
+            </div>
+        </article>
     )
 }
